98ce1cabedef78a12c47a0d50a89b344a76e26ab
renoweebeloy-prog <renowee.beloy@dorsu.edu.ph>
2026-05-20 13:59:00 +0800

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once __DIR__ . '/../app/User.php';
-require_once __DIR__ . '/../app/helpers.php';
+require_once __DIR__ . '/app/User.php';
+require_once __DIR__ . '/app/helpers.php';
 require_login();
 
 $user = User::find((int) current_user()['id']) ?: current_user();
