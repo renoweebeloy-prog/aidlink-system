d2e17827cd99cdc090ab32da6cd5e0d7d28542f2
renoweebeloy-prog <renowee.beloy@dorsu.edu.ph>
2026-05-20 13:15:36 +0800

    Update api_counts.php

--- api_counts.php
+++ api_counts.php
@@ -1,16 +1,26 @@
 <?php
 session_start();
-require_once __DIR__ . '/../app/helpers.php';
-require_once __DIR__ . '/../app/Notification.php';
-require_once __DIR__ . '/../app/Messenger.php';
+
+require_once __DIR__ . '/helpers.php';
+require_once __DIR__ . '/Notification.php';
+require_once __DIR__ . '/Messenger.php';
+
 header('Content-Type: application/json');
+
 $user = current_user();
+
 if (!$user) {
-    echo json_encode(['ok' => false, 'message_count' => 0, 'notification_count' => 0]);
+    echo json_encode([
+        'ok' => false,
+        'message_count' => 0,
+        'notification_count' => 0
+    ]);
     exit;
 }
+
 echo json_encode([
     'ok' => true,
     'message_count' => Messenger::unreadCount((int) $user['id']),
-    'notification_count' => Notification::unreadCount((int) $user['id']),
+    'notification_count' => Notification::unreadCount((int) $user['id'])
 ]);
+exit;
