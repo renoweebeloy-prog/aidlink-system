465e5562f0c1b93f56a3db9d83b8f7dc646e9ab2
renoweebeloy-prog <renowee.beloy@dorsu.edu.ph>
2026-05-20 14:00:06 +0800

    Update users.php

--- users.php
+++ users.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once __DIR__ . '/../app/User.php';
-require_once __DIR__ . '/../app/helpers.php';
+require_once __DIR__ . '/app/User.php';
+require_once __DIR__ . '/app/helpers.php';
 require_role(['admin']);
 
 $error = '';
