1b819d53c2c8b4563561a39759903a7e7d00149f
renoweebeloy-prog <renowee.beloy@dorsu.edu.ph>
2026-05-20 13:58:33 +0800

    Update reset_password.php

--- reset_password.php
+++ reset_password.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once __DIR__ . '/../app/Auth.php';
-require_once __DIR__ . '/../app/helpers.php';
+require_once __DIR__ . '/app/Auth.php';
+require_once __DIR__ . '/app/helpers.php';
 
 $error = '';
 $success = '';
