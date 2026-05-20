ff5b4f13db1befd007f7d2d1c432a1ca862c216a
renoweebeloy-prog <renowee.beloy@dorsu.edu.ph>
2026-05-20 13:29:49 +0800

    Update Database.php

--- Database.php
+++ Database.php
@@ -11,35 +11,22 @@ class Database
         }
 
         $config = require __DIR__ . '/config.php';
+        $db = $config['db'];
 
-        $dsn = sprintf(
-            'mysql:host=%s;port=%s;dbname=%s;charset=utf8mb4',
-            $config['db']['host'],
-            $config['db']['port'],
-            $config['db']['name']
-        );
+        $dsn = "mysql:host={$db['host']};port={$db['port']};dbname={$db['name']};charset=utf8mb4";
 
         try {
-
-            self::$connection = new PDO(
-                $dsn,
-                $config['db']['user'],
-                $config['db']['pass'],
-                [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                ]
-            );
+            self::$connection = new PDO($dsn, $db['user'], $db['pass'], [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]);
 
             return self::$connection;
 
         } catch (PDOException $error) {
-
             http_response_code(500);
-
             echo "<h2>Database Connection Failed</h2>";
             echo "<p>" . $error->getMessage() . "</p>";
-
             exit;
         }
     }
