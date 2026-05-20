2ba84b394ad29d929f378e6a3f8f39add7cc0451
renoweebeloy-prog <renowee.beloy@dorsu.edu.ph>
2026-05-20 13:59:33 +0800

    Update theme.php

--- theme.php
+++ theme.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once __DIR__ . '/../app/helpers.php';
-require_once __DIR__ . '/../app/User.php';
+require_once __DIR__ . '/app/helpers.php';
+require_once __DIR__ . '/app/User.php';
 
 header('Content-Type: application/json');
 
