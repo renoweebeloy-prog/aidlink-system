2e293bec393c765e1b5cbae54a6ddb0c8a14eb77
renoweebeloy-prog <renowee.beloy@dorsu.edu.ph>
2026-05-21 08:44:40 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once __DIR__ . '/../app/Auth.php';
+require_once __DIR__ . '/app/Auth.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['confirm_logout'])) {
 
