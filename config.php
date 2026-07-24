2d800b8ec355abf08ea9c11559e0b8d4a54472e8
renoweebeloy-prog <renowee.beloy@dorsu.edu.ph>
2026-07-24 21:40:04 +0800

    Update config.php

--- config.php
+++ config.php
@@ -3,12 +3,10 @@
 return [
     'app_name' => 'AidLink',
     'db' => [
-        // Mukuha ni sa Render Environment Variables, pero kung wala, 
-        // gamiton niya kining imong default Supabase credentials.
-        'host' => getenv('DB_HOST') ?: 'db.jfaqeporbfqacwklvhov.supabase.co',
+        'host' => getenv('DB_HOST') ?: 'aws-0-ap-southeast-1.pooler.supabase.com',
         'name' => getenv('DB_NAME') ?: 'postgres',
-        'user' => getenv('DB_USER') ?: 'postgres',
+        'user' => getenv('DB_USER') ?: 'postgres.jfaqeporbfqacwklvhov',
         'pass' => getenv('DB_PASS') ?: 'D3GNEtgqZezUV9b9',
-        'port' => getenv('DB_PORT') ?: 5432,
+        'port' => getenv('DB_PORT') ?: 6543,
     ]
 ];
